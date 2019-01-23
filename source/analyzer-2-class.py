5c344905e8a65b102722ecd5781c1e777d2ac6c3
Andrew Forrester <awforrester@gmail.com>
2019-01-23 11:54:26 -0800

    Update source, initializing new branch.

--- source/analyzer-2-class.py
+++ source/analyzer-2-class.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 # filename: analyzer-2-class.py
 
+# Source for code modification
+# Training and Deploying A Deep Learning Model in Keras MobileNet V2
+#  and Heroku: A Step-by-Step Tutorial Part 1
+# https://hackernoon.com/tf-serving-keras-mobilenetv2-632b8d92983c
+
 import keras
 from keras.applications.mobilenetv2 import MobileNetV2
 from keras.layers import Dense, Input, Dropout
 from keras.models import Model
+
+# dimension of (pre-processed) images
+# (when expressed as numpy.ndarray after conversion from jpg)
+target_size = 576
+
 
 def build_model():
     input_tensor = Input(shape=(target_size, target_size, 3))
@@ -16,21 +26,23 @@ def build_model():
         pooling='avg')
 
     for layer in base_model.layers:
-        layer.trainable = True  # trainable has to be false in order to freeze the layers
+        layer.trainable = True
+        # trainable has to be false in order to freeze the layers
 
     op = Dense(256, activation='relu')(base_model.output)
     op = Dropout(.25)(op)
 
-    ##
-    # softmax: calculates a probability for every possible class.
-    #
+    # softmax: good for exclusive classifications
     # activation='softmax': return the highest probability;
-    # for example, if 'Coat' is the highest probability then the result would be 
-    # something like [0,0,0,0,1,0,0,0,0,0] with 1 in index 5 indicate 'Coat' in our case.
-    ##
+    # for example, if class-4 is the highest probability then the result
+    # would be something like [0,0,0,0,1,0,0,0,0,0] with 1 in index 4
     output_tensor = Dense(10, activation='softmax')(op)
 
     model = Model(inputs=input_tensor, outputs=output_tensor)
 
     return model
+
+
+
+build_model()
 
