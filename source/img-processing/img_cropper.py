f89bbf33bedf6408adafcfaffdf5e2ed8faa2137
Andrew Forrester <awforrester@gmail.com>
2019-02-11 04:52:55 -0800

    Add functionality to img_cropper. Used it to process data set B.

--- source/img-processing/img_cropper.py
+++ source/img-processing/img_cropper.py
@@ -2,6 +2,7 @@
 # filename: img_cropper.py
 
 from PIL import Image, ImageOps
+import os
 
 def crop_pil_img(pil_img):
     border = (32, 32, 32, 32) # left, top, right, bottom
@@ -13,7 +14,25 @@ def crop_n_save_img(img_filepath,cropped_filepath):
     cropped_pil_img = crop_pil_img(pil_img)
     cropped_pil_img.save(cropped_filepath)
 
-def crop_n_save_all_imgs_in_folder(folder_path):
-#   Need to have a robust naming system across many folders.
+#def crop_n_save_all_imgs_in_folder(folder_path):
+#   In general, need to have a robust naming system across many folders.
 #   crop_n_save_img()
+
+
+src_path_base = '../../input-data/1-pre-processed/B/'
+dst_path_base = '../../input-data/2-processed/B/'
+i = 0
+for dirPath, subDirs, files in os.walk(src_path_base+'Normal/'):
+        for file in files:
+            if file.endswith(".jpg"):
+                i += 1
+                crop_n_save_img( os.path.join(dirPath, file) ,
+                                 dst_path_base+f'Normal/{i:02d}.jpg' )
+i = 0
+for dirPath, subDirs, files in os.walk(src_path_base+'Abnormal/'):
+        for file in files:
+            if file.endswith(".jpg"):
+                i += 1
+                crop_n_save_img( os.path.join(dirPath, file) ,
+                                 dst_path_base+f'Abnormal/{i:02d}.jpg' )
 
