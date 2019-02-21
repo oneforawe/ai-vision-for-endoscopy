5c3c383e270c3709a0f0b7b7852f8238904bd824
Andrew Forrester <awforrester@gmail.com>
2019-02-21 13:03:54 -0800

    Fix data split problem. (Code had assumed an even split between Normal and Abnormal, which is not true for data set E.)

--- source/data_loader.py
+++ source/data_loader.py
@@ -28,17 +28,36 @@ def load_data_2class_abnormality(path):
     dfn['abnormality'] = 0
     dfa['abnormality'] = 1
 
+    # check
+    print(f'len(dfn) = {len(dfn)}')
+    print(f'len(dfa) = {len(dfa)}')
+    print('')
+    print(f'len(normalImgFiles) = {len(normalImgFiles)}')
+    print(f'len(abnormalImgFiles) = {len(abnormalImgFiles)}')
+    print('')
+    print(f'(used)  splitPoint = int(0.8*len(abnormalImgFiles)) = {int(0.8*len(abnormalImgFiles))}')
+    print(f'(check) splitPoint = int(0.8*len(dfa)) = {int(0.8*len(dfa))}')
+
     # Split into train and test sets.
     # (train will be further split into train/learn-validation folds with KFold)
-    splitPoint = int(0.8*len(abnormalImgFiles))
-    n_train = dfn[:splitPoint]
-    n_test  = dfn[splitPoint:]
-    a_train = dfa[:splitPoint]
-    a_test  = dfa[splitPoint:]
+    normal_splitPoint   = int(0.8*len(normalImgFiles))
+    abnormal_splitPoint = int(0.8*len(abnormalImgFiles))
+    n_train = dfn[:normal_splitPoint]
+    print(f'len(n_train) = {len(n_train)}')
+    n_test  = dfn[normal_splitPoint:]
+    print(f'len(n_test) = {len(n_test)}')
+    a_train = dfa[:abnormal_splitPoint]
+    print(f'len(a_train) = {len(a_train)}')
+    a_test  = dfa[abnormal_splitPoint:]
+    print(f'len(a_test) = {len(a_test)}')
     df_train = pd.concat([n_train,a_train])
+    print(f'len(df_train) = {len(df_train)}')
     df_test  = pd.concat([n_test,a_test])
+    print(f'len(df_test) = {len(df_test)}')
     df_train = shuffle(df_train).reset_index(drop=True)
+    print(f'len(df_train) = {len(df_train)}')
     df_test  = shuffle(df_test).reset_index(drop=True)
+    print(f'len(df_test) = {len(df_test)}')
 
     train_set    = df_train
     train_labels = np.array(train_set['abnormality'].iloc[: ])
@@ -47,6 +66,10 @@ def load_data_2class_abnormality(path):
     test_set    = df_test
     test_labels = np.array(test_set['abnormality'].iloc[: ])
     test_files  = df_test['filepath'].tolist()
+
+    # check
+    print(f'len(train_set) = {len(train_set)}')
+    print(f'len(test_set) = {len(test_set)}')
 
     return train_set, train_files, train_labels, test_set, test_files
 
