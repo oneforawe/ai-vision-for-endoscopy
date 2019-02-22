9796b3c18926015e7f9f844a103f884d592e8a53
Andrew Forrester <awforrester@gmail.com>
2019-02-22 07:50:25 -0800

    Ready for experimental training of mobilenet d on data D.

--- source/analyzer_breeder.py
+++ source/analyzer_breeder.py
@@ -33,13 +33,13 @@ def main():
     #data_name = 'data_B'
     #data_short_name = 'B'
     # C: 200 images. (Abnormal=Blood)
-    data_path = data_base+'C'
-    data_name = 'data_C'
-    data_short_name = 'C'
+    #data_path = data_base+'C'
+    #data_name = 'data_C'
+    #data_short_name = 'C'
     # D: 2000 images.
-    #data_path = data_base+'D'
-    #data_name = 'data_D'
-    #data_short_name = 'D'
+    data_path = data_base+'D'
+    data_name = 'data_D'
+    data_short_name = 'D'
     # E: 10000 images.
     #data_path = data_base+'E'
     #data_name = 'data_E'
@@ -99,14 +99,14 @@ def main():
                                 f'{data_category}/{class_split}/{data_name}/'
 
     # Prepare for training
-    batch_size = 4  # C
-    #batch_size = 20 # D (mobilenet)
+    #batch_size = 4  # C
+    batch_size = 20 # D (mobilenet)
     #batch_size = 10 # D (xception) #Got error (fixed by reducing to 10)
     #batch_size = 10 #E (mobilenet) #Got error at 40, reducing to 10
     #batch_size = 100 # F
     epochs = 50
-    n_fold = 4 # C
-    #n_fold = 5 # D,E,F
+    #n_fold = 4 # C
+    n_fold = 5 # D,E,F
     histories = []
 
     # Find this run's number ("train-and-test run #..")
