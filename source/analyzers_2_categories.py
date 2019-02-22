4105395f8b4795251fbea87cfc63530120e2ace8
Andrew Forrester <awforrester@gmail.com>
2019-02-22 07:11:31 -0800

    Ready for more experimental training of mobilenet d on data C.

--- source/analyzers_2_categories.py
+++ source/analyzers_2_categories.py
@@ -142,7 +142,7 @@ def mobilenet_v2_d(img_dim):
     #x  = Dense( 7, kernel_regularizer=regularizers.l1_l2(l1=0.01, l2=0.01) )(x)
     #x  = Dense(7, kernel_regularizer=regularizers.l1_l2(l1=0.01, l2=0.01),
     #              activity_regularizer=regularizers.l1_l2(l1=0.01, l2=0.01))(x)
-    x  = Dense(10, kernel_regularizer=regularizers.l2(0.01),
+    x  = Dense(10, kernel_regularizer=regularizers.l2(0.02),
                    activity_regularizer=regularizers.l1(0.01))(x)
     xo = Dense(1, activation='sigmoid')(x) # output tensor
     model = Model(inputs=xi, outputs=xo, name='mobilenet_v2_d')
